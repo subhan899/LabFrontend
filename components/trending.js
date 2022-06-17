d6b38b29e37bcb260e41cc659a4df53de2682c9e
ahmad subhan <subhan.08ahmad@gmail.com>
2022-06-17 22:58:10 +0700

    lab

--- components/trending.js
+++ components/trending.js
@@ -60,7 +60,7 @@ const Trending = () => {
          <Image src="/alif.png" alt="card image"  height="auto"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>THE FATE OF THE FURIOUS 8</Card.Text>
-         <Card.Text>Bagus 9.5/10</Card.Text>
+         <Card.Text>Bagus 10.0/10</Card.Text>
          </div>
 
        </Card>
